b43f7d57aeb91d32da26f3a0d296abde26b134b9
azatuuluaman <azatuuluaman@mail.ru>
2022-12-29 14:18:58 +0600

    db fix

--- Bitrix/settings.py
+++ Bitrix/settings.py
@@ -84,10 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': "postgres",
-        'USER':"postgres",
+        'USER': "postgres",
         'PASSWORD': "postgres",
         'HOST': "db",
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
